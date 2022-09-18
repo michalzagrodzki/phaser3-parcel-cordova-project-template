7384ed48bcd21e27cd18c89f7d45518ee2f384b8
Michal Zagrodzki <michalzagrodzki@gmail.com>
2022-09-18 20:31:11 +0200

    feat: add game content from phaser library

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+import Phaser from "phaser";
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-var cordova = document.createElement('script')
-cordova.setAttribute('type','text/javascript')
-cordova.setAttribute('src', 'cordova.js')
-document.getElementsByTagName("head")[0].appendChild(cordova)
+import sceneGame from './game'
 
-document.addEventListener('deviceready', onDeviceReady, false);
+const cordova = document.createElement('script');
+cordova.setAttribute('type','text/javascript');
+cordova.setAttribute('src', 'cordova.js');
+document.getElementsByTagName("head")[0].appendChild(cordova);
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+const config = {
+  type: Phaser.AUTO,
+  parent: "phaser-cordova",
+  width: window.outerWidth,
+  height: window.outerHeight,
+  render: {
+    antialias: false,
+    pixelArt: true,
+    roundPixels: true
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false
+    }
+  },
+  scale: {
+    mode: Phaser.Scale.FIT
+  },
+  scene: [
+    sceneGame
+  ]
+};
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-    console.log('Application is ready to serve')
-}
+export default new Phaser.Game(config);
