3e6a0db21eaba3aac487014278c0dfe6204e37bd
Michal Zagrodzki <michalzagrodzki@gmail.com>
2022-11-24 20:51:46 +0100

    fix: correct methods for running phaser in parcel and cordova

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import Phaser from "phaser";
 import sceneGame from './game'
 
-const loadCordova = document.createElement('script');
-loadCordova.setAttribute('type','text/javascript');
-loadCordova.setAttribute('src', 'cordova.js');
-document.getElementsByTagName("head")[0].appendChild(loadCordova);
-
-document.addEventListener('deviceready', onDeviceReady, false);
-
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-}
-
 const config = {
   type: Phaser.AUTO,
   parent: "phaser-cordova",
@@ -39,4 +26,20 @@ const config = {
   ]
 };
 
-export default new Phaser.Game(config);
+if (process.env.NODE_ENV === 'development'){
+  new Phaser.Game(config);
+} else if (process.env.NODE_ENV === 'production') {
+  const loadCordova = document.createElement('script');
+  loadCordova.setAttribute('type','text/javascript');
+  loadCordova.setAttribute('src', 'cordova.js');
+  document.getElementsByTagName("head")[0].appendChild(loadCordova);
+  document.addEventListener('deviceready', onDeviceReady, false);
+
+  function onDeviceReady() {
+      // Cordova is now initialized. Have fun!
+      new Phaser.Game(config);
+      console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+      document.getElementById('deviceready').classList.add('ready');
+  }
+}
+
