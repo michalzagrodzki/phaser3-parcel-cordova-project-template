ba0ce11b4d5bbf308ed43a7f46e2cf6f96599ac7
Michal Zagrodzki <michalzagrodzki@gmail.com>
2022-12-30 21:41:00 +0100

    feat: correct build options file

--- scripts/beforeBuild.js
+++ scripts/beforeBuild.js
@@ -3,14 +3,14 @@ const fs = require('fs');
 const rf = require('rimraf');
 
 const options = {
-  //production: true,
+  production: true,
   outDir: './www', // The out directory to put the build files in, defaults to dist
   // publicUrl: './', // The url to serve on, defaults to '/'
   watch: false, // Whether to watch the files and rebuild them on change, defaults to process.env.NODE_ENV !== 'production'
   // cache: false, // Enabled or disables caching, defaults to true
   contentHash: false, // Disable content hash from being included on the filename
   // global: 'moduleName', // Expose modules as UMD under this name, disabled by default
-  // minify: false, // Minify files, enabled if process.env.NODE_ENV === 'production'
+  minify: false, // Minify files, enabled if process.env.NODE_ENV === 'production'
   // target: 'browser', // Browser/node/electron, defaults to browser
   bundleNodeModules: true, 
   sourceMaps: false, 
