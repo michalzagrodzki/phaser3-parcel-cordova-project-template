af2568ecc18ed68b5150cf8219f72755aa4e4ac1
Michal Zagrodzki <michalzagrodzki@gmail.com>
2022-12-31 17:01:37 +0100

    fix: correct config

--- scripts/beforeBuild.js
+++ scripts/beforeBuild.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 const rf = require('rimraf');
 
 const options = {
-  production: true,
   outDir: './www', // The out directory to put the build files in, defaults to dist
   // publicUrl: './', // The url to serve on, defaults to '/'
   watch: false, // Whether to watch the files and rebuild them on change, defaults to process.env.NODE_ENV !== 'production'
@@ -12,9 +11,9 @@ const options = {
   // global: 'moduleName', // Expose modules as UMD under this name, disabled by default
   minify: false, // Minify files, enabled if process.env.NODE_ENV === 'production'
   // target: 'browser', // Browser/node/electron, defaults to browser
-  bundleNodeModules: true, 
-  sourceMaps: false, 
-  detailedReport: true 
+  bundleNodeModules: true,
+  sourceMaps: false,
+  detailedReport: true
 };
 
 const bundler = new Bundler('src/index.html', options);
